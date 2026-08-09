33109ff3f1ff14e532a323fda05dc5fb4f759c3b
Basavaraj <chanduharugeri@gmail.com>
2026-08-09 17:57:26 +0530

    Improve homepage SEO for Bengaluru

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -10,23 +10,66 @@ import WhatsAppButton from "../components/WhatsAppButton";
 import SEO from "../components/SEO";
 
 function Home() {
+  const structuredData = {
+    "@context": "https://schema.org",
+    "@type": "LocalBusiness",
+    "@id": "https://www.rebornhomecare.in/#organization",
+    name: "Reborn Home Care",
+    url: "https://www.rebornhomecare.in/",
+    logo: "https://www.rebornhomecare.in/favicon.svg",
+    description:
+      "Reborn Home Care provides professional home nursing, patient care, elder care, physiotherapy, wound care and other home healthcare services in Bengaluru.",
+    telephone: "+917829094405",
+    areaServed: {
+      "@type": "City",
+      name: "Bengaluru",
+    },
+    serviceType: [
+      "Home Nursing Care",
+      "Patient Care",
+      "Elder Care",
+      "Physiotherapy",
+      "Wound Care",
+      "Diabetes Care",
+      "Dementia Care",
+      "Cancer Care",
+    ],
+  };
+
   return (
     <>
       <SEO
-        title="Home Healthcare & Nursing Services"
-        description="Reborn Home Care provides professional home nursing, patient care, elder care, physiotherapy, wound care and other home healthcare services in Bengaluru, Karnataka."
+        title="Home Nursing & Elder Care Services in Bengaluru | Reborn Home Care"
+        description="Reborn Home Care provides professional home nursing, patient care, elder care, physiotherapy, wound care and other healthcare services at home in Bengaluru."
         path="/"
-        image="/images/about.jpg"
+      />
+
+      {/* Local Business Structured Data */}
+      <script
+        type="application/ld+json"
+        dangerouslySetInnerHTML={{
+          __html: JSON.stringify(structuredData),
+        }}
       />
 
       <Navbar />
-      <Hero />
-      <Services />
-      <WhyChooseUs />
-      <Stats />
-      <Testimonials />
-      <FAQ />
+
+      <main>
+        <Hero />
+
+        <Services />
+
+        <WhyChooseUs />
+
+        <Stats />
+
+        <Testimonials />
+
+        <FAQ />
+      </main>
+
       <Footer />
+
       <WhatsAppButton />
     </>
   );
